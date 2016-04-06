20671815b5e57df55f225a66eda2552ff3eed654
foodpedaler <kiefer.timothy@gmail.com>
2016-04-06 22:16:12 +0000

    add noir effect

--- videobooth.js
+++ videobooth.js
@@ -1,4 +1,5 @@
 var videos = {video1: "video/demovideo1", video2: "video/demovideo2"};
+var effectFunction = null;
 
 window.onload = function() {
   
@@ -21,14 +22,11 @@ window.onload = function() {
     videoLinks[i].onclick = setVideo;
   }
   
+  video.addEventListener("ended", endedHandler, false);
+  video.addEventListener("play", processFrame, false);
+  
   pushUnpushButtons("video1", []);
   pushUnpushButtons("video2", []);
-  
-  video.addEventListener("ended", endedHandler, false);
-}
-
-function endedHandler() {
-  pushUnpushButtons("", ["play"]);
 }
 
 function handleControl(e) {
@@ -69,13 +67,25 @@ function setEffect(e) {
   
   if (id == "normal") {
     pushUnpushButtons("normal", ["western", "noir", "scifi"]);
+    effectFunction = null;
   } else if (id == "western") {
     pushUnpushButtons("western", ["normal", "noir", "scifi"]);
+    effectFunction = western;
   } else if (id == "noir") {
     pushUnpushButtons("noir", ["normal", "western", "scifi"]);
+    effectFunction = noir;
   } else if (id == "scifi") {
     pushUnpushButtons("scifi", ["normal", "western", "noir"]);
+    effectFunction = scifi;
   }
+}
+
+function noir(pos, r, g, b, data) {
+	var brightness = (3*r + 4*g + b) >>> 3;
+	if (brightness < 0) brightness = 0;
+	data[pos * 4 + 0] = brightness;
+	data[pos * 4 + 1] = brightness;
+	data[pos * 4 + 2] = brightness;
 }
 
 
@@ -133,3 +143,35 @@ function getFormatExtension() {
     return ".ogv";
   }
 }
+
+function endedHandler() {
+  pushUnpushButtons("", ["play"]);
+}
+
+function processFrame() {
+  var video = document.getElementById("video");
+  if (video.paused || video.ended) {
+    return;
+  }
+  var bufferCanvas = document.getElementById("buffer");
+  var displayCanvas = document.getElementById("display");
+  var buffer = bufferCanvas.getContext("2d");
+  var display = displayCanvas.getContext("2d");
+  
+  buffer.drawImage(video, 0, 0, bufferCanvas.width, bufferCanvas.height);
+  var frame = buffer.getImageData(0, 0, bufferCanvas.width, bufferCanvas.height);
+  
+  var length = frame.data.length / 4;
+  
+  for (var i = 0; i < length; i++) {
+    var r = frame.data[i * 4 + 0];
+    var g = frame.data[i * 4 + 1];
+    var b = frame.data[i * 4 + 2];
+    if (effectFunction) {
+      effectFunction(i, r, g, b, frame.data);
+    }
+  }
+  display.putImageData(frame, 0, 0);
+  setTimeout(processFrame, 0);
+}
+
